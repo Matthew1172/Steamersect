e4224caae34159772c07236ffb7635e046c57ae7
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-07-07 01:42:20 +0000

    removed tests in main

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ my_key = os.environ['STEAM_TOKEN']
 
 my_id = "76561198012762732"
 
+
 def get_player_details(steam_id):
     summary_r = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}".format(
         my_key, steam_id)
     result = requests.get(summary_r)
     return result.json()
+
 
 def get_friend_ids(steam_id):
     friend_r = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={}&steamid={}&relationship=friend".format(
@@ -21,6 +23,7 @@ def get_friend_ids(steam_id):
         friend_ids.append(i["steamid"])
     return friend_ids
 
+
 def get_owned_games(steam_id):
     games_r = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}&format=json".format(
         my_key, steam_id)
@@ -30,9 +33,11 @@ def get_owned_games(steam_id):
     else:
         return []
 
+
 def get_app_details(appid):
     get_app_details_call = "https://store.steampowered.com/api/appdetails?appids={}"
     return requests.get(get_app_details_call.format(appid)).json()
+
 
 def enter_steam_ids():
     flag = False
@@ -45,17 +50,23 @@ def enter_steam_ids():
             flag = True
     return player_ids
 
+
 '''
 pre: ids is a list of steam ids we want to operate on. length of the list is less than 200.
 post: return a 2d list where each element of the list is a list of games for the person respective to their index
 '''
+
+
 def get_games_from_ids(player_ids):
     return [get_owned_games(player_id) for player_id in player_ids]
+
 
 '''
 pre: input a 2d list of players games
 post: output a dictionary of the union of all games and number of people that have the game
 '''
+
+
 def build_game_map(list_of_games):
     total = {}
     for player in list_of_games:
@@ -66,12 +77,17 @@ def build_game_map(list_of_games):
                 total[game] = 1
     return total
 
+
 def filter(list_of_games, a):
     '''
     :param list_of_games: a 2d list of games where the outer list are players and the inner list are their owned games
     :return: a list of games ids where 50% or more of all players own the game
     '''
-    return [k for k, v in build_game_map(list_of_games).items() if a(len(list_of_games), v)]
+    return [
+        k for k, v in build_game_map(list_of_games).items()
+        if a(len(list_of_games), v)
+    ]
+
 
 def get_game_details(list_of_games):
     '''
@@ -80,14 +96,23 @@ def get_game_details(list_of_games):
     '''
     if len(list_of_games) > 200: j = 200
     else: j = len(list_of_games)
-    return [requests.get("https://store.steampowered.com/api/appdetails?appids={}".format(app)).json() for app in list_of_games[:j]]
+    return [
+        requests.get(
+            "https://store.steampowered.com/api/appdetails?appids={}".format(
+                app)).json() for app in list_of_games[:j]
+    ]
+
 
 def get_game_names(list_of_game_details):
     '''
     :param list_of_game_details: list of steam game objects
     :return: list of game names
     '''
-    return [v['data']['name'] for game in list_of_game_details if game is not None for k, v in game.items() if v['success']]
+    return [
+        v['data']['name'] for game in list_of_game_details if game is not None
+        for k, v in game.items() if v['success']
+    ]
+
 
 def main():
     # ids = enter_steam_ids()
@@ -126,7 +151,4 @@ def interface_majority(list_of_steam_ids):
 
 
 if __name__ == '__main__':
-    print(interface_majority([76561198012762732, 76561198024725398]))
-    print(interface_intersect([76561198012762732, 76561198024725398]))
-
     pass
